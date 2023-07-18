30f9f954c326abb5bddfc5f70d9aff598f37cb7d
Laura Lopez <lauraamelina@gmail.com>
2023-07-18 12:44:40 -0300

    Test Auth API Login & Register

--- __tests__/login.test.ts
+++ __tests__/login.test.ts
@@ -3,16 +3,46 @@ import { app } from '../src'
 describe('Auth API', () => {
   it('should register a user', async () => {
     const response = await request(app)
-      .post('/register')
-      .send({ email: 'testuser@test.com', password: 'testpass', name: 'Test User', role: ['admin'] })
+      .post('/auth/register')
+      .send({ name: 'Test User', email: 'testt@test.com', password: 'testpass', roles: [1] })
     expect(response.status).toBe(200)
-    expect(response.body).toHaveProperty('message', 'User registered successfully')
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: false,
+        data: {
+          id: expect.any(Number),
+          name: 'Test User',
+          email: 'testt@test.com',
+          password: expect.any(String),
+          roles: [
+            {
+              id: 1,
+              name: 'Admin',
+              permissions: expect.arrayContaining([
+                expect.objectContaining({
+                  id: expect.any(Number),
+                  method: expect.any(String),
+                  route: expect.any(String),
+                }),
+              ]),
+            },
+          ],
+        },
+      })
+    )
   })
-  it('should log in a user', async () => {
+  it('should login a user', async () => {
     const response = await request(app)
-      .post('/login')
-      .send({ email: 'testuser@test.com', password: 'testpass' })
+      .post('/auth/login')
+      .send({ email: 'testt@test.com', password: 'testpass' })
     expect(response.status).toBe(200)
-    expect(response.body).toHaveProperty('message', 'User logged in successfully')
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: false,
+        data: {
+          token: expect.any(String),
+        },
+      })
+    )
   })
 })
