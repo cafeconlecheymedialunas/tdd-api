31c913fcc375a0caefef141a7ea4bde54d2fbcaf
Mauro Gaitan <maurodeveloper86@gmail.com>
2023-07-24 15:16:50 -0300

    Refactor names y files equals and delete Functional naming Classes

--- src/domain/types/response.ts
+++ src/domain/types/response.ts
@@ -1,5 +1,18 @@
 import { Permission } from '../entities/Permission';
 
+export enum Condition {
+  Equal = 'equal',
+  NotEqual = 'not_equal',
+  GreaterThan = 'greater_than',
+  LessThan = 'less_than',
+}
+
+export type QueryFilter = {
+  key: string;
+  condition: Condition;
+  value: string;
+};
+
 export interface Payload {
   id: number;
   permissions: Permission[];
