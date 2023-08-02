ae474fb70aa52faf18625d8a6f413665d0493e3a
Mauro <maurodeveloper86@gmail.com>
2023-08-02 15:27:11 -0300

    prevent use return any types

--- src/domain/types/responseOutputs.ts
+++ src/domain/types/responseOutputs.ts
@@ -1,5 +1,9 @@
-import { Permission as PermissionEntity } from '../entities/Permission';
-
+import { Role as RoleDto} from "../../application/dtos/Role";
+import { User as UserDto} from "../../application/dtos/User";
+import { Permission as PermissionDto } from "../../application/dtos/Permission";
+import { User as UserEntity } from "../entities/User";
+import { Role as RoleEntity } from "../entities/Role";
+import { Permission as PermissionEntity } from "../entities/Permission";
 export enum Condition {
   Equal = 'equal',
   NotEqual = 'not_equal',
@@ -22,3 +26,6 @@ export interface ValidationError {
   key: string;
   error: string;
 }
+
+export type Dtos = UserDto | RoleDto | PermissionDto
+export type Entities = UserEntity | RoleEntity | PermissionEntity
