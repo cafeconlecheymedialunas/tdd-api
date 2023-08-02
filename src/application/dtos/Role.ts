3200687e7c599181f060f43e865f9884aeca1026
Mauro <maurodeveloper86@gmail.com>
2023-08-02 15:23:17 -0300

    Always return a dto

--- src/application/dtos/Role.ts
+++ src/application/dtos/Role.ts
@@ -1,4 +1,4 @@
-import { Permission as PermissionEntity } from '../../domain/entities/Permission';
+import { Permission as PermissionDto } from "./Permission";
 /**
  * This DTO is the return of all methods from the Role Repository. It includes the 'permissions' property that will be transformed by Role Mapper in a Permission[] array.
  * @class Role
@@ -6,9 +6,9 @@ import { Permission as PermissionEntity } from '../../domain/entities/Permission
 export class Role {
   id: number;
   name: string;
-  permissions: PermissionEntity[];
+  permissions: PermissionDto[];
 
-  constructor(name: string, id: number, permissions: PermissionEntity[]) {
+  constructor(name: string, id: number, permissions: PermissionDto[]) {
     this.name = name;
 
     this.id = id;
