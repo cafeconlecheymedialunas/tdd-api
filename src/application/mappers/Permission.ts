c89ad986478264e07aac706772b7b8a6776f0b22
Laura Lopez <lauraamelina@gmail.com>
2023-08-10 16:03:07 -0300

    JSDoc Permission Mapper

--- src/application/mappers/Permission.ts
+++ src/application/mappers/Permission.ts
@@ -1,8 +1,16 @@
-import { Permissionable as PermissionMaperable } from '../../domain/interfaces/mappers/Permissionable';
+import { Permissionable as PermissionMapperable } from '../../domain/interfaces/mappers/Permissionable';
 import { Permission as PermissionEntity } from '../../domain/entities/Permission';
 import { Permission as PermissionDto } from '../dtos/Permission';
 
-export class Permission implements PermissionMaperable {
+/**
+ * Class representing a Permission mapper.
+ */
+export class Permission implements PermissionMapperable {
+  /**
+   * Converts a PermissionEntity object into a PermissionDto object.
+   * @param {PermissionEntity} permission - The PermissionEntity object to be mapped.
+   * @returns {PermissionDto} - The resulting PermissionDto object.
+   */
   mapItem = (permission: PermissionEntity): PermissionDto => {
     return {
       id: permission.id,
@@ -11,6 +19,11 @@ export class Permission implements PermissionMaperable {
     };
   };
 
+  /**
+   * Converts a list of PermissionEntity objects into a list of PermissionDto objects.
+   * @param {PermissionEntity[]} permissions - The list of PermissionEntity objects to be mapped.
+   * @returns {PermissionDto[]} - The resulting list of PermissionDto objects.
+   */
   mapList = (permissions: PermissionEntity[]): PermissionDto[] => {
     const results = permissions.map((item: PermissionEntity) => this.mapItem(item));
 
