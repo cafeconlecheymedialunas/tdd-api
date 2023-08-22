1a2823d82b94cc5a0ced55b524671daf54c5ed52
Laura Lopez <lauraamelina@gmail.com>
2023-08-22 15:55:32 -0300

    Users Controller

--- src/infrastructure/controllers/users.ts
+++ src/infrastructure/controllers/users.ts
@@ -1,8 +1,32 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { response } from '../utils';
+import { GetAllUsers } from '../../application/useCases/GetAllUsers';
+import { User as UserDataMapper } from '../../application/mappers/User';
+import { Role as RoleDataMapper } from '../../application/mappers/Role';
+import { Permission as PermissionDataMapper } from '../../application/mappers/Permission';
+import { UserMock } from '../repositories/UserMock';
+import { RoleMock } from '../repositories/RoleMock';
+import { PermissionMock } from '../repositories/PermissionMock';
+import { Mock } from '../repositories/Mock';
+import { User as UserEntity } from '../../domain/entities/User';
 
-const users = (req: Request, res: Response): void => {
-  return response(res, 200, { route: '/users', method: 'GET' });
+const getAllUsersUseCase = new GetAllUsers(
+  new UserMock(
+    new UserDataMapper(new RoleMock(new RoleDataMapper(new PermissionMock(new PermissionDataMapper())))),
+    new Mock<UserEntity>(),
+  ),
+);
+
+const getAllUsers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const users = await getAllUsersUseCase.run();
+
+    if (!users) response(res, 200, []);
+
+    return response(res, 200, users);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export default users;
+export default getAllUsers;
